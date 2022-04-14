99e0a02ccbead3cc6af3d0841c7f6c8e46ca76da
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-04-15 00:00:59 +0800

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+export { default as Anchor } from './components/Anchor';
+export { default as Box } from './components/Box';
+export { default as Button } from './components/Button';
+export { default as Flex } from './components/Flex';
+export { default as Icon } from './components/Icon';
+export { default as Test } from './components/Test';
+export {
+  default as Text,
+  EM,
+  Strong,
+  Heading,
+  H1,
+  H2,
+  H3,
+  H4,
+} from './components/Typography';
+export { default as VisuallyHidden } from './components/VisuallyHidden';
+
+export {
+  Shadows,
+  styled,
+  css,
+  keyframes,
+  getCssText,
+  theme,
+  config,
+} from './lib/stitches.config';
+export { globalStyles } from './lib/globalStyles';
+export type { CSS, VariantProps } from './lib/stitches.config';
