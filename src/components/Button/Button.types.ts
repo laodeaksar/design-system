dbc8c495d969a4c84a27f2f4184a75a63ab19d83
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-05-27 11:59:31 +0800

    Update Button.types.ts

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -4,7 +4,7 @@ export type MainButtonVariant = 'primary' | 'secondary';
 export type IconButtonVariant = 'icon';
 
 interface BaseButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-  // Note @MaximeHeckel: I honestly gave up on typing this properly
+  // Note @ode_aksar: I honestly gave up on typing this properly
   // Originally I wanted to type it as ForwardRefComponent<HTMLButtonElement, HTMLMotionProps<"button">>;
   // But that is somehow invalid :shurg. The objective here would be to only allow polymorphism for motion.button
   as?: ForwardRefComponent<HTMLButtonElement, any>;
