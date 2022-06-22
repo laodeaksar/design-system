8fd292c191ef19af80bf26bca97d27c5bb837f69
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-22 13:11:32 +0800

    Create sizes.ts

---
+++ src/lib/tokens/sizes.ts
@@ -0,0 +1,25 @@
+const sizes = {
+  0: '0px',                                          
+  1: '4px',
+  2: '8px',
+  3: '12px',
+  4: '16px',
+  5: '24px',
+  6: '32px',
+  7: '40px',
+  8: '48px',                                          
+  9: '56px',                                          
+  10: '64px',
+  11: '80px',                                         
+  12: '96px',
+  13: '128px',
+  14: '256px',
+  15: '512px',
+  full: '100%',
+  'w-screen': '100vw',
+  'h-screen': '100vh',
+  max: 'max-content',
+  min: 'min-content'
+};
+
+export { sizes }
