8af08bfe3f6749c1d0efdb843151fa279b1e76a7
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-23 07:47:28 +0800

    Update Button.styles.tsx

--- src/components/Button/Button.styles.tsx
+++ src/components/Button/Button.styles.tsx
@@ -1,155 +1,193 @@
-import { styled } from 'src/lib/stitches.config';
+import { styled } from '@/lib/stitches.config';
 
 export const StyledButton = styled('button', {
-  WebkitAppearance: 'none',
-  WebkitTapHighlightColor: 'transparent',
-  WebkitUserSelect: 'none',
-  MozUserSelect: 'none',
-  MsUserSelect: 'none',
-  userSelect: 'none',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexShrink: '0',
-  outline: 'none',
-  cursor: 'pointer',
-  border: '0',
-  font: 'inherit',
-  /* Constant properties */
-  fontSize: 'var(--font-size-2)',
-  fontWeight: 'var(--font-weight-3)',
-  height: '44px',
-  width: 'max-content',
-  padding: '11px 16px',
-  transition: 'background 0.2s, transform 0.2s, color 0.2s, box-shadow 0.3s',
-  borderRadius: 'var(--border-radius-1)',
-  /* Computed properties */
-  background: 'var(--background, white)',
-  color: 'var(--color, black)',
-  transform: 'scale(var(--button-scale, 1)) translateZ(0)',
-  boxShadow: 'var(--shadow, none)',
-  opacity: 'var(--opacity, 1)',
-  '--shadow-hover-primary':
-    '0 2px 40px -4px var(--laodeaksar-form-input-focus)',
+    $$background: 'white',
+    $$scale: 1,
+    $$shadow: 'none',
+    $$opacity: 1,
+    $$color: 'black',
 
-  '&:active': {
-    '--button-scale': '0.95',
-  },
-  '&:disabled': {
-    cursor: 'not-allowed',
-  },
+    WebkitAppearance: 'none',
+    WebkitTapHighlightColor: 'transparent',
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexShrink: '0',
+    outline: 'none',
+    cursor: 'pointer',
+    border: '0',
+    font: 'inherit',
 
-  variants: {
-    variant: {
-      primary: {
-        '--background': 'var(--laodeaksar-colors-brand)',
-        '--color': 'hsl(var(--palette-gray-00))',
-        '&:disabled': {
-          '--background': 'var(--laodeaksar-form-input-disabled)',
-          '--color': 'var(--laodeaksar-colors-typeface-tertiary)',
-        },
+    /* Constant properties */
+    fontSize: '$2',
+    fontWeight: '$3',
+    height: '44px',
+    width: '$max',
+    padding: '11px 16px',
+    transition: 'background 0.2s, transform 0.3s, color 0.2s, box-shadow 0.3s',
+    borderRadius: '$1',
+    background: '$$background',
+    color: '$$color',
+    transform: 'scale($$scale) translateZ(0)',
+    boxShadow: '$$shadow',
+    opacity: '$$opacity',
 
-        '&:hover': {
-          '&:not(:disabled)': {
-            '--shadow': 'var(--shadow-hover-primary)',
-          },
-        },
+    '$$shadow-hover-primary':
+        '0 2px 40px -4px var(--laodeaksar-form-input-focus)',
 
-        '&:focus-visible': {
-          '--shadow': 'var(--shadow-hover-primary)',
-        },
-      },
-      secondary: {
-        '--background': 'var(--laodeaksar-colors-emphasis)',
-        '--color': 'var(--laodeaksar-colors-brand)',
-        '&:disabled': {
-          '--background': 'var(--laodeaksar-form-input-disabled)',
-          '--color': 'var(--laodeaksar-colors-typeface-tertiary)',
-        },
-
-        '&:hover': {
-          '&:not(:disabled)': {
-            '--shadow': 'var(--shadow-hover-primary)',
-          },
-        },
-
-        '&:focus-visible': {
-          '--shadow': 'var(--shadow-hover-primary)',
-        },
-      },
+    '&:active': {
+        $$scale: '0.95'
     },
-  },
+
+    '&:disabled': {
+        cursor: 'not-allowed'
+    },
+
+    variants: {
+        variant: {
+            primary: {
+                $$background: 'var(--laodeaksar-colors-brand)',
+                $$color: 'hsl(var(--palette-gray-00))',
+
+                '&:disabled': {
+                    $$background: 'var(--laodeaksar-form-input-disabled)',
+                    $$color: 'var(--laodeaksar-colors-typeface-tertiary)'
+                },
+
+                '&:hover': {
+                    '&:not(:disabled)': {
+                        $$shadow: '$$shadow-hover-primary'
+                    }
+                },
+
+                '&:focus-visible': {
+                    $$shadow: '$$shadow-hover-primary'
+                }
+            },
+
+            secondary: {
+                $$background: 'var(--laodeaksar-colors-emphasis)',
+                $$color: 'var(--laodeaksar-colors-brand)',
+
+                '&:disabled': {
+                    $$background: 'var(--laodeaksar-form-input-disabled)',
+                    $$color: 'var(--laodeaksar-colors-typeface-tertiary)'
+                },
+
+                '&:hover': {
+                    '&:not(:disabled)': {
+                        $$shadow: '$$shadow-hover-primary'
+                    }
+                },
+
+                '&:focus-visible': {
+                    $$shadow: '$$shadow-hover-primary'
+                }
+            },
+            danger: {
+                $$background: 'var(--laodeaksar-colors-emphasis)',
+                $$color: 'var(--laodeaksar-colors-danger)',
+
+                '&:disabled': {
+                    $$background: 'var(--laodeaksar-form-input-disabled)',
+                    $$color: 'var(--laodeaksar-colors-typeface-tertiary)'
+                },
+
+                '&:hover': {
+                    '&:not(:disabled)': {
+                        $$shadow: '$$shadow-hover-primary'
+                    }
+                },
+
+                '&:focus-visible': {
+                    $$shadow: '$$shadow-hover-primary'
+                }
+            }
+        }
+    }
 });
 
 export const StyledIconButton = styled('button', {
-  WebkitAppearance: 'none',
-  WebkitTapHighlightColor: 'transparent',
-  WebkitUserSelect: 'none',
-  MozUserSelect: 'none',
-  MsUserSelect: 'none',
-  userSelect: 'none',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexShrink: '0',
-  outline: 'none',
-  cursor: 'pointer',
-  border: '0',
-  /* Constant properties */
-  width: '44px',
-  height: '44px',
-  background: 'transparent',
-  transition: 'color 0.3s ease, transform 0.3s ease',
-  borderRadius: 'var(--border-radius-1)',
-  color: 'var(--color, var(--laodeaksar-colors-typeface-tertiary))',
-  transform: 'scale(var(--button-content-scale, 1)) translateZ(0)',
-  '--shadow-hover-primary':
-    '0 2px 40px -4px var(--laodeaksar-form-input-focus)',
+    $$color: 'var(--laodeaksar-colors-typeface-tertiary)',
+    $$scale: 1,
 
-  '&::after': {
-    zIndex: '0',
-    position: 'absolute',
-    content: "''",
-    display: 'block',
-    width: '100%',
-    height: '100%',
-    borderRadius: 'var(--corner, var(--border-radius-1))',
-    transition:
-      'box-shadow 0.3s ease, border-color 0.2s, background 0.3s ease,\n      transform 0.3s cubic-bezier(0.34, 1.56, 0.64, 1)',
-    background: 'var(--background, var(--laodeaksar-colors-foreground))',
-    transform: 'scale(var(--button-background-scale, 1)) translateZ(0)',
-    border:
-      'var(--border-thickness, 1px) solid var(--border-color, transparent)',
-    boxShadow: 'var(--shadow, none)',
-  },
+    WebkitAppearance: 'none',
+    WebkitTapHighlightColor: 'transparent',
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexShrink: '0',
+    outline: 'none',
+    cursor: 'pointer',
+    border: '0',
 
-  '&:disabled': {
-    cursor: 'not-allowed',
-    '--background': 'var(--laodeaksar-form-input-disabled)',
-    '--color': 'var(--laodeaksar-colors-typeface-tertiary)',
-  },
+    /* Constant properties */
+    size: '44px',
+    background: 'transparent',
+    transition: 'color 0.3s ease, transform 0.3s ease',
+    borderRadius: '$1',
+    color: '$$color',
+    transform: 'scale($$scale) translateZ(0)',
 
-  '&:hover': {
-    '&:not(:disabled)': {
-      '--border-color': 'var(--laodeaksar-colors-brand)',
-      '--border-thickness': '2px',
-      '--color': 'var(--laodeaksar-colors-brand)',
-      '--corner': 'calc(var(--border-radius-1) + 2px)',
-      '--button-background-scale': '0.92',
-      '--shadow': 'var(--shadow-hover-primary)',
+    '$$shadow-hover-primary':
+        '0 2px 40px -2px var(--laodeaksar-form-input-focus)',
+
+    '&::after': {
+        $$background: 'var(--laodeaksar-colors-foreground)',
+        $$afterscale: 1,
+        $$thickness: '1px',
+        $$border: 'transparent',
+        $$shadow: 'none',
+
+        zIndex: '0',
+        position: 'absolute',
+        content: '""',
+        display: 'block',
+        size: '$full',
+        borderRadius: '$2',
+        transition:
+            'box-shadow 0.3s ease, border-color 0.2s, background 0.3s ease,\n      transform 0.3s, cubic-bezier(0.34, 1.56, 0.64, 1)',
+        background: '$$background',
+        transform: 'scale($$afterscale) translateZ(0)',
+        border: '$$thickness solid $$border',
+        boxShadow: '$$shadow'
     },
-  },
 
-  '&:focus-visible': {
-    '--border-color': 'var(--laodeaksar-colors-brand)',
-    '--border-thickness': '2px',
-    '--color': 'var(--laodeaksar-colors-brand)',
-    '--corner': 'calc(var(--border-radius-1) + 2px)',
-    '--button-background-scale': 0.92,
-    '--shadow': 'var(--shadow-hover-primary)',
-  },
+    '&:disabled': {
+        cursor: 'not-allowed',
+        $$background: 'var(--laodeaksar-form-input-disabled)',
+        $$color: 'var(--laodeaksar-colors-typeface-tertiary)'
+    },
 
-  '&:active': {
-    '--button-content-scale': '0.95',
-  },
+    '&:hover': {
+        '&:not(:disabled)': {
+            $$border: 'var(--laodeaksar-colors-brand)',
+            $$thickness: '2px',
+            $$color: 'var(--laodeaksar-colors-brand)',
+            $$corner: 'calc($space$2) + 2px)',
+            $$afterscale: '0.92',
+            $$shadow: '$$shadow-hover-primary'
+        }
+    },
+
+    '&:focus-visible': {
+        $$border: 'var(--laodeaksar-colors-brand)',
+        $$thickness: '2px',
+        $$color: 'var(--laodeaksar-colors-brand)',
+        $$corner: 'calc($space$2) + 2px)',
+        $$afterscale: '0.92',
+        $$shadow: '$$shadow-hover-primary'
+    },
+
+    '&:active': {
+        $$scale: '0.95'
+    }
 });
