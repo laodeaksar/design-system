a0cc1a12a1cd2796505567c757ae3499ba236c2a
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-22 13:14:02 +0800

    Update typography.ts

--- src/lib/tokens/typography.ts
+++ src/lib/tokens/typography.ts
@@ -1,31 +1,46 @@
-export const fonts = {
-  '--font-fallback':
-    "-apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  '--font-display': "'Inter', var(--font-fallback)",
-  '--font-mono': "'Fira Code', monospace",
-  '--font-numeric': "'Space Grotesk', var(--font-fallback)",
-};
+const fonts = {
+  default: 'inherit',
+  display:
+    'IBM Plex Sans, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+  mono: 'Iosevka, menlo, monospace',
+  numeric: 'IBM Plex Sans'
+}
 
-export const fontSizes = {
-  /* 
-    Note: we set --font-size-1 in px on purpose: 14px is the smallest font we should allow in the UI 
-    to pass mobile usability issue. Setting it to REM would shrink it to 12px on mobile which is 
-    too small
-    */
-  '--font-size-1': '14px;' /*0.75rem*/,
-  '--font-size-2': '0.875rem' /* 16px */,
-  '--font-size-3': '1rem',
-  '--font-size-4': '1.125rem',
-  '--font-size-5': '1.25rem',
-  '--font-size-6': '1.5rem',
-  '--font-size-7': '2rem',
-};
+const fontSizes = {
+  1: '14px',
+  2: '0.875rem',
+  3: '1rem',
+  4: '1.125rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  7: '2rem',
+  8: '2.25rem',
+  9: '2.5rem'
+}
 
-export const fontWeights = {
-  '--font-weight-1': '300',
-  '--font-weight-2': '400',
-  '--font-weight-3': '500',
-  '--font-weight-4': '600',
-};
+const lineHeights = {
+  none: 1,
+  tight: 1.25,
+  snug: 1.375,
+  normal: 1.5,
+  relaxed: 1.625,
+  loose: 2
+}
 
-/* TODO: Define line height tokens with `--leading` */
+const letterSpacings = {
+  tighter: '-0.05em',
+  tight: '-0.025em',
+  normal: '0em',
+  wide: '0.025em',
+  wider: '0.05em',
+  widest: '0.1em'
+}
+
+const fontWeights = {
+  1: '300',
+  2: '400',
+  3: '500',
+  4: '600'
+}
+
+export {fonts, fontSizes, fontWeights, lineHeights, letterSpacings, }
