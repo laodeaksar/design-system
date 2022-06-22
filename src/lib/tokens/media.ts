aa28a867ce894a9b395f48a6bb89a15841da8244
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-22 13:09:59 +0800

    Create media.ts

---
+++ src/lib/tokens/media.ts
@@ -0,0 +1,11 @@
+const media = {                                          
+  sm: '(min-width: 640px)',                       
+  md: '(min-width: 768px)',
+  lg: '(min-width: 1024px)',
+  motion: '(prefers-reduced-motion)',
+  hover: '(hover: hover) and (pointer: fine)',
+  dark: '(prefers-color-scheme: dark)',
+  light: '(prefers-color-scheme: light)'
+}
+
+export { media }
