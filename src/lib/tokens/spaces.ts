1c6a97767f0e01dbb7158ded8b00c93c138215c4
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-22 13:12:59 +0800

    Update spaces.ts

--- src/lib/tokens/spaces.ts
+++ src/lib/tokens/spaces.ts
@@ -1,18 +1,21 @@
-export const spaces = {
-  '--space-0': '0',
-  '--space-1': '4px',
-  '--space-2': '8px',
-  '--space-3': '12px',
-  '--space-4': '16px',
-  '--space-5': '24px',
-  '--space-6': '32px',
-  '--space-7': '40px',
-  '--space-8': '48px',
-  '--space-9': '56px',
-  '--space-10': '64px',
-  '--space-11': '80px',
-  '--space-12': '96px',
-  '--space-13': '128px',
-  '--space-14': '256px',
-  '--space-15': '512px',
+const spaces = {
+  0: '0px',
+  1: '4px',
+  2: '8px',
+  3: '12px',
+  4: '16px',
+  5: '24px',
+  6: '32px',
+  7: '40px',
+  8: '48px',
+  9: '56px',
+  10: '64px',
+  11: '80px',
+  12: '96px',
+  13: '128px',
+  14: '256px',
+  15: '512px',
+  auto: 'auto'
 };
+
+export { spaces }
