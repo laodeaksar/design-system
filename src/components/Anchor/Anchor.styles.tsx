29c709e7c8fe5457de56167f232431df0f38b765
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-22 13:56:16 +0800

    Update Anchor.styles.tsx

--- src/components/Anchor/Anchor.styles.tsx
+++ src/components/Anchor/Anchor.styles.tsx
@@ -1,34 +1,35 @@
 import { styled } from 'src/lib/stitches.config';
 
 export const StyledAnchor = styled('a', {
+  $$color: 'var(--laodeaksar-colors-brand)',
+
   fontSize: 'inherit',
   lineHeight: 'inherit',
-  color: 'var(--color, var(--laodeaksar-colors-brand))',
-  fontWeight: 500,
+  color: '$$color',
+  fontWeight: '$3',
   wordBreak: 'break-word',
-  textDecoration: 'none',
   outline: 'none',
   transition: 'border-color 0.3s ease, color 0.3s ease',
 
   '--hover-color': 'var(--laodeaksar-colors-typeface-primary)',
 
   '&:focus': {
-    '--color': 'var(--hover-color, var(--laodeaksar-colors-brand))',
-    '--hover-translation-distance': 'var(--arrow-translation, 0)',
+    $$color: 'var(--hover-color, var(--laodeaksar-colors-brand))',
+    '--hover-translation-distance': 'var(--arrow-translation, 0)'
   },
 
-  '@media (hover: hover) and (pointer: fine)': {
+  '@hover': {
     '&:hover': {
-      '--color': 'var(--hover-color, var(--laodeaksar-colors-brand))',
-      ' --hover-translation-distance': 'var(--arrow-translation, 0)',
-    },
+      $$color: 'var(--hover-color, var(--laodeaksar-colors-brand))',
+      '--hover-translation-distance': 'var(--arrow-translation, 0)'
+    }
   },
 
   variants: {
     discreet: {
       true: {
-        '--color': 'var(--laodeaksar-colors-typeface-tertiary)',
-      },
+        $$color: 'var(--laodeaksar-colors-typeface-tertiary)'
+      }
     },
     arrow: {
       left: {
@@ -38,60 +39,59 @@ export const StyledAnchor = styled('a', {
         '--hover-color': 'unset',
 
         '&:before': {
-          content: '',
+          content: '""',
           display: 'inline-block',
           verticalAlign: 'middle',
-          width: 'var(--size, 1.05em)',
-          height: 'var(--size, 1.05em)',
+          size: 'var(--size, 1.05em)',
           maskImage: 'var(--icon)',
           WebkitMaskRepeat: 'no-repeat',
           backgroundColor: 'currentColor',
           marginRight: '0.18em',
           transition: 'transform 0.4s ease',
           transform:
-            'translateY(-2px) translateX(var(--hover-translation-distance, 0px)) scaleX(var(--arrow-direction, 1))',
-        },
+            'translateY(-1px) translateX(var(--hover-translation-distance, 0px)) scaleX(var(--arrow-direction, 1))'
+        }
       },
       right: {
         '--size': '1.1em',
-        '--arrow-direction': 1,
         '--arrow-translation': '0.25em',
+        '--arrow-direction': 1,
         '--hover-color': 'unset',
 
         '&:after': {
-          content: '',
+          content: '""',
           display: 'inline-block',
           verticalAlign: 'middle',
-          width: 'var(--size, 1.05em)',
-          height: 'var(--size, 1.05em)',
+          size: 'var(--size, 1.05em)',
           maskImage: 'var(--icon)',
           WebkitMaskRepeat: 'no-repeat',
           backgroundColor: 'currentColor',
           marginLeft: '0.18em',
           transition: 'transform 0.4s ease',
           transform:
-            'translateY(-2px) translateX(var(--hover-translation-distance, 0px)) scaleX(var(--arrow-direction, 1))',
-        },
-      },
+            'translateY(-2px) translateX(var(--hover-translation-distance, 0px)) scaleX(var(--arrow-direction, 1))'
+        }
+      }
     },
+
     favicon: {
       true: {
         '--size': '1.1em',
 
         '&:before': {
-          content: '',
+          content: '""',
           display: 'inline-block',
           verticalAlign: 'middle',
-          width: 'var(--size, 1.05em)',
-          height: 'var(--size, 1.05em)',
+          size: 'var(--size, 1.05em)',
           maskImage: 'var(--icon)',
           WebkitMaskRepeat: 'no-repeat',
           backgroundColor: 'currentColor',
           marginRight: '0.18em',
-          transform: 'translateY(-2px)',
-        },
-      },
+          transform: 'translateY(-2px)'
+        }
+      }
     },
+
     underline: {
       true: {
         borderBottom: '2px solid',
@@ -100,15 +100,33 @@ export const StyledAnchor = styled('a', {
         '--hover-color': 'unset',
 
         '&:focus': {
-          '--border-color': 'hsl(var(--palette-blue-40))',
+          '--border-color': 'hsl(var(--palette-blue-40))'
         },
 
-        '@media (hover: hover) and (pointer: fine)': {
+        '@hover': {
           '&:hover': {
-            '--border-color': 'hsl(var(--palette-blue-40))',
-          },
-        },
-      },
+            '--border-color': 'hsl(var(--palette-blue-40))'
+          }
+        }
+      }
     },
-  },
+    tags: {
+      true: {
+        '--size': '1.1em',
+        border: '1px solid currentColor',
+
+        '&:before': {
+          content: '""',
+          display: 'inline-block',
+          verticalAlign: 'middle',
+          size: 'var(--size, 1.05em)',
+          maskImage: 'var(--icon)',
+          WebkitMaskRepeat: 'no-repeat',
+          backgroundColor: 'currentColor',
+          marginRight: '0.18em',
+          transform: 'translateY(-2px)'
+        }
+      }
+    }
+  }
 });
