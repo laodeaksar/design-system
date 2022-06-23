05ade3ef92dae554a0d09b3649742a34117656b3
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-23 08:12:31 +0800

    Update InlineCode.styles.tsx

--- src/components/InlineCode/InlineCode.styles.tsx
+++ src/components/InlineCode/InlineCode.styles.tsx
@@ -1,15 +1,15 @@
-import { Shadows, styled } from 'src/lib/stitches.config';
+import { Shadows, styled } from '@/lib/stitches.config';
 
 export const StyledInlineCode = styled('code', {
-  fontFamily: 'var(--font-mono)',
+  fontFamily: '$mono',
   lineHeight: '1.45rem',
-  borderRadius: 'var(--border-radius-1)',
+  borderRadius: '$1',
   backgroundColor: 'var(--laodeaksar-colors-foreground)',
   color: 'var(--token-keyword)',
   padding: '2px 8px',
-  fontSize: 'var(--font-size-2)',
-  fontWeight: 'var(--font-weight-2) !important',
+  fontSize: '$2',
+  fontWeight: '$2 !important',
   wordBreak: 'break-word',
   border: '1px solid var(--laodeaksar-border-color)',
-  boxShadow: Shadows[1],
+  boxShadow: Shadows[1]
 });
