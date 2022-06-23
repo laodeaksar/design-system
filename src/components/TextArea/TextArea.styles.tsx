20df4ddcef33ffccfc49215fc0a719f069e039be
laodeaksarr <101698503+laodeaksarr@users.noreply.github.com>
2022-06-23 14:48:42 +0800

    Update TextArea.styles.tsx

--- src/components/TextArea/TextArea.styles.tsx
+++ src/components/TextArea/TextArea.styles.tsx
@@ -1,78 +1,87 @@
-import { Shadows, styled } from 'src/lib/stitches.config';
+import { styled } from '@/lib/stitches.config';
 
 export const StyledTextArea = styled('textarea', {
-  WebkitAppearance: 'none',
-  MozAppearance: 'none',
+  $$border: 'var(--laodeaksar-form-input-border)',
+  $$background: 'var(--laodeaksar-form-input-background)',
+  $$cursor: 'initial',
+  $$opacity: '1',
+  $$shadow: 'none',
 
-  width: '100%',
+  appearance: 'none',
+
+  width: '$full',
   outline: 'none',
   position: 'relative',
   display: 'block',
   margin: 0,
   padding: '8px 16px',
 
-  fontSize: 'var(--font-size-2)',
+  fontSize: '$2',
   fontFamily: 'inherit',
   lineHeight: '26px',
   color: 'var(--laodeaksar-colors-typeface-primary)',
 
-  borderRadius: 'var(--border-radius-1)',
-  border: '1px solid var(--border-color, var(--laodeaksar-form-input-border))',
-  background: 'var(--background, var(--laodeaksar-form-input-background))',
-  cursor: 'var(--cursor, initial)',
-  opacity: 'var(--opacity, 1)',
-  boxShadow: 'var(--shadow, none)',
+  borderRadius: '$1',
+  border: '1px solid $$border',
+  background: '$$background',
+  cursor: '$$cursor',
+  opacity: '$$opacity',
+  boxShadow: '$$shadow',
   transition: 'border-color 0.3s, box-shadow 0.3s',
 
-  '--shadow-hover-primary':
+  '$$shadow-hover-primary':
     '0 2px 20px -2px var(--laodeaksar-form-input-focus)',
 
   '&::placeholder': {
     color: 'var(--laodeaksar-colors-typeface-tertiary)',
-    opacity: 0.5,
+    opacity: 0.5
   },
 
   '&::-webkit-autofill': {
-    background: 'transparent',
+    background: 'transparent'
   },
 
   '&:disabled': {
-    '--background': 'var(--laodeaksar-form-input-disabled)',
+    $$background: 'var(--laodeaksar-form-input-disabled)',
     cursor: 'not-allowed',
     opacity: 0.65,
+
     '& + label': {
-      cursor: 'not-allowed',
-    },
+      cursor: 'not-allowed'
+    }
   },
 
   '&:hover': {
     '&:not(:disabled)': {
-      '--border-color': 'var(--laodeaksar-form-input-active)',
-      '--shadow': 'var(--shadow-hover-primary)',
-    },
+      $$border: 'var(--laodeaksar-form-input-active)',
+      $$shadow: '$$shadow-hover-primary'
+    }
   },
 
-  '&:focus': {
-    '--border-color': 'var(--laodeaksar-form-input-active)',
-    '--shadow': 'var(--shadow-hover-primary)',
+  '&:focus-within': {
+    $$border: 'var(--laodeaksar-form-input-active)',
+    $$shadow: '$$shadow-hover-primary'
   },
 
   variants: {
     readOnly: {
       true: {
-        '--cursor': 'default',
-      },
+        $$cursor: 'default'
+      }
     },
+
     resize: {
       none: {
-        resize: 'none',
+        resize: 'none'
       },
+
       vertical: {
-        resize: 'vertical',
+        resize: 'vertical'
       },
+
       horizontal: {
-        resize: 'horizontal',
-      },
-    },
-  },
+        resize: 'horizontal'
+      }
+    }
+  }
 });
