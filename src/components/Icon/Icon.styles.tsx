83b8342df9f231f99fa31479a3a06e3a9a92e68a
bahutara <108091324+bahutara@users.noreply.github.com>
2022-07-12 01:47:17 +0800

    remove getVariant

--- src/components/Icon/Icon.styles.tsx
+++ src/components/Icon/Icon.styles.tsx
@@ -1,52 +1,98 @@
-import { getVariant, styled, VariantProps } from '@/lib/stitches.config';
+import { styled, VariantProps } from "@/lib/stitches.config";
 
-export const StyledSVG = styled('svg', {
+export const StyledSVG = styled("svg", {
   variants: {
     variant: {
-      default: { stroke: 'currentColor', fill: 'none' },
+      default: { stroke: "currentColor", fill: "none" },
       primary: {
-        stroke: 'var(--laodeaksar-colors-typeface-primary)',
-        fill: 'none',
+        stroke: "var(--laodeaksar-colors-typeface-primary)",
+        fill: "none",
       },
       secondary: {
-        stroke: 'var(--laodeaksar-colors-typeface-secondary)',
-        fill: 'none',
+        stroke: "var(--laodeaksar-colors-typeface-secondary)",
+        fill: "none",
       },
       tertiary: {
-        stroke: 'var(--laodeaksar-colors-typeface-tertiary)',
-        fill: 'none',
+        stroke: "var(--laodeaksar-colors-typeface-tertiary)",
+        fill: "none",
       },
       info: {
-        stroke: 'var(--laodeaksar-colors-brand)',
-        fill: 'var(--laodeaksar-colors-emphasis)',
+        stroke: "var(--laodeaksar-colors-brand)",
+        fill: "var(--laodeaksar-colors-emphasis)",
       },
       danger: {
-        stroke: 'var(--laodeaksar-colors-danger)',
-        fill: 'var(--laodeaksar-colors-danger-emphasis)',
+        stroke: "var(--laodeaksar-colors-danger)",
+        fill: "var(--laodeaksar-colors-danger-emphasis)",
       },
       success: {
-        stroke: 'var(--laodeaksar-colors-success)',
-        fill: 'var(--laodeaksar-colors-success-emphasis)',
+        stroke: "var(--laodeaksar-colors-success)",
+        fill: "var(--laodeaksar-colors-success-emphasis)",
       },
       warning: {
-        stroke: 'var(--laodeaksar-colors-warning)',
-        fill: 'var(--laodeaksar-colors-warning-emphasis)',
+        stroke: "var(--laodeaksar-colors-warning)",
+        fill: "var(--laodeaksar-colors-warning-emphasis)",
       },
       none: {},
     },
     outline: {
       true: {
-        fill: 'none !important',
+        fill: "none !important",
       },
     },
-    size: getVariant('sizes', (token) => ({ size: token })),
+    size: {
+      1: {
+        size: "$1",
+      },
+      2: {
+        size: "$2",
+      },
+      3: {
+        size: "$3",
+      },
+      4: {
+        size: "$4",
+      },
+      5: {
+        size: "$5",
+      },
+      6: {
+        size: "$6",
+      },
+      7: {
+        size: "$7",
+      },
+      8: {
+        size: "$8",
+      },
+      9: {
+        size: "$9",
+      },
+      10: {
+        size: "$10",
+      },
+      11: {
+        size: "$11",
+      },
+      12: {
+        size: "$12",
+      },
+      13: {
+        size: "$13",
+      },
+      14: {
+        size: "$14",
+      },
+      15: {
+        size: "$15",
+      },
+    },
   },
   defaultVariants: {
-    variant: 'default',
+    variant: "default",
     outline: true,
     size: 5,
   },
 });
 
-export type IconSize = VariantProps<typeof StyledSVG>['size'];
-export type IconVariant = VariantProps<typeof StyledSVG>['variant'];
+export type IconSize = VariantProps<typeof StyledSVG>["size"];
+export type IconVariant = VariantProps<typeof StyledSVG>["variant"];
