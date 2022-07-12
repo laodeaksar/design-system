7384f2d320b8b89e84c8498f274974cb81f68ffc
bahutara <akhsaras23@gmail.com>
2022-07-12 12:18:46 +0000

    fix

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -1,142 +1,50 @@
-import { styled } from 'src/lib/stitches.config';
+import { getVariant, styled } from 'src/lib/stitches.config';
 
-const Flex = styled('div', {
+import Box from 'src/components/Box';
+
+const Flex = styled(Box, {
   display: 'flex',
 
   variants: {
     alignItems: {
-      baseline: {
-        alignItems: 'baseline',
-      },
-      center: {
-        alignItems: 'center',
-      },
-      end: {
-        alignItems: 'end',
-      },
-      ['flex-end']: {
-        alignItems: 'flex-end',
-      },
-      ['flex-start']: {
-        alignItems: 'flex-start',
-      },
-      start: {
-        alignItems: 'start',
-      },
-      stretch: {
-        alignItems: 'stretch',
-      },
+      baseline: { alignItems: 'baseline' },
+      center: { alignItems: 'center' },
+      end: { alignItems: 'end' },
+      ['flex-end']: { alignItems: 'flex-end' },
+      ['flex-start']: { alignItems: 'flex-start' },
+      start: { alignItems: 'start' },
+      stretch: { alignItems: 'stretch' },
     },
     alignContent: {
-      baseline: {
-        alignContent: 'baseline',
-      },
-      center: {
-        alignContent: 'center',
-      },
-      end: {
-        alignContent: 'end',
-      },
-      start: {
-        alignContent: 'start',
-      },
-      stretch: {
-        alignContent: 'stretch',
-      },
+      baseline: { alignContent: 'baseline' },
+      center: { alignContent: 'center' },
+      end: { alignContent: 'end' },
+      start: { alignContent: 'start' },
+      stretch: { alignContent: 'stretch' },
     },
     direction: {
-      column: {
-        flexDirection: 'column',
-      },
-      columnReverse: {
-        flexDirection: 'column-reverse',
-      },
-      row: {
-        flexDirection: 'row',
-      },
-      rowReverse: {
-        flexDirection: 'row-reverse',
-      },
+      column: { flexDirection: 'column' },
+      ['column-reverse']: { flexDirection: 'column-reverse' },
+      row: { flexDirection: 'row' },
+      ['row-reverse']: { flexDirection: 'row-reverse' },
     },
-    gap: {
-      1: {
-        gap: 'var(--space-1)',
-      },
-      2: {
-        gap: 'var(--space-2)',
-      },
-      3: {
-        gap: 'var(--space-3)',
-      },
-      4: {
-        gap: 'var(--space-4)',
-      },
-      5: {
-        gap: 'var(--space-5)',
-      },
-      6: {
-        gap: 'var(--space-6)',
-      },
-      7: {
-        gap: 'var(--space-7)',
-      },
-      8: {
-        gap: 'var(--space-8)',
-      },
-      9: {
-        gap: 'var(--space-9)',
-      },
-      10: {
-        gap: 'var(--space-10)',
-      },
-      11: {
-        gap: 'var(--space-11)',
-      },
-      12: {
-        gap: 'var(--space-12)',
-      },
-      13: {
-        gap: 'var(--space-13)',
-      },
-      14: {
-        gap: 'var(--space-14)',
-      },
-      15: {
-        gap: 'var(--space-15)',
-      },
-    },
+    gap: getVariant('space', (token) => ({ gap: token })),
     justifyContent: {
-      center: {
-        justifyContent: 'center',
-      },
-      end: {
-        justifyContent: 'end',
-      },
-      ['space-around']: {
-        justifyContent: 'space-around',
-      },
-      ['space-between']: {
-        justifyContent: 'space-between',
-      },
-      ['space-evenly']: {
-        justifyContent: 'space-evenly',
-      },
-      start: {
-        justifyContent: 'start',
-      },
+      center: { justifyContent: 'center' },
+      end: { justifyContent: 'end' },
+      ['space-around']: { justifyContent: 'space-around' },
+      ['space-between']: { justifyContent: 'space-between' },
+      ['space-evenly']: { justifyContent: 'space-evenly' },
+      start: { justifyContent: 'start' },
     },
     wrap: {
-      wrap: {
-        flexWrap: 'wrap',
-      },
-      nowrap: {
-        flexWrap: 'nowrap',
-      },
+      true: { flexWrap: 'wrap' },
+      false: { flexWrap: 'nowrap' },
     },
   },
   defaultVariants: {
     gap: 1,
-    wrap: 'nowrap',
+    wrap: false,
     direction: 'row',
     alignItems: 'center',
     justifyContent: 'flex-start',
