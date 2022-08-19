6e39caf32d199070a86837be8786afc012dd68f2
laodeaksarr <akhsarodhe@gmail.com>
2022-08-19 07:31:24 +0000

    TextInput forwardRef

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -8,74 +8,78 @@ import { StyledInput, StyledInputWrapper } from './TextInput.styles';
 import type { TextInputProps } from './TextInput.types';
 import { validateEmail } from './utils';
 
-const TextInput = (props: TextInputProps) => {
-  const {
-    id,
-    disabled,
-    label,
-    type = 'text',
-    placeholder,
-    value,
-    ...rest
-  } = props;
+const TextInput = React.forwardRef(
+  (props: TextInputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
+    const {
+      id,
+      disabled,
+      label,
+      type = 'text',
+      placeholder,
+      value,
+      ...rest
+    } = props;
 
-  const [showPassword, setShowPassword] = React.useState(false);
-  const isValid = React.useMemo(() => validateEmail(value || ''), [value]);
+    const [showPassword, setShowPassword] = React.useState(false);
+    const isValid = React.useMemo(() => validateEmail(value || ''), [value]);
 
-  const computedType = React.useCallback(() => {
-    if (type === 'password' && showPassword) {
-      return 'text';
-    }
-    return type;
-  }, [showPassword, type]);
+    const computedType = React.useCallback(() => {
+      if (type === 'password' && showPassword) {
+        return 'text';
+      }
+      return type;
+    }, [showPassword, type]);
 
-  return (
-    <StyledInputWrapper className={isValid ? 'valid' : ''} variant={type}>
-      {label && (
-        <Label
-          htmlFor={id}
-          style={{
-            marginBottom: '8px',
-          }}
-        >
-          {label}
-        </Label>
-      )}
-      <Box css={{ position: 'relative' }}>
-        <StyledInput
-          id={id}
-          className={isValid ? 'valid' : ''}
-          disabled={disabled}
-          // TODO cleanup variants: variants != types
-          variant={computedType()}
-          type={computedType()}
-          placeholder={placeholder}
-          value={value}
-          {...rest}
-        />
-        {type === 'email' && (
-          <>
-            <AtSignIcon />
-            <Tick />
-          </>
+    return (
+      <StyledInputWrapper className={isValid ? 'valid' : ''} variant={type}>
+        {label && (
+          <Label
+            htmlFor={id}
+            style={{
+              marginBottom: '8px',
+            }}
+          >
+            {label}
+          </Label>
         )}
+        <Box css={{ position: 'relative' }}>
+          <StyledInput
+            id={id}
+            className={isValid ? 'valid' : ''}
+            disabled={disabled}
+            // TODO cleanup variants: variants != types
+            variant={computedType()}
+            type={computedType()}
+            placeholder={placeholder}
+            value={value}
+            {...rest}
+          />
+          {type === 'email' && (
+            <>
+              <AtSignIcon />
+              <Tick />
+            </>
+          )}
 
-        {type === 'password' && (
-          <>
-            <button
-              aria-label="Reveal Password"
-              className={showPassword ? 'clicked' : ''}
-              data-testid="reveal-password-button"
-              disabled={disabled}
-              onClick={() => setShowPassword((prev) => !prev)}
-            >
-              <EyeIcon />
-            </button>
-          </>
-        )}
-      </Box>
-    </StyledInputWrapper>
-  );
-};
+          {type === 'password' && (
+            <>
+              <button
+                aria-label="Reveal Password"
+                className={showPassword ? 'clicked' : ''}
+                data-testid="reveal-password-button"
+                disabled={disabled}
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                <EyeIcon />
+              </button>
+            </>
+          )}
+        </Box>
+      </StyledInputWrapper>
+    );
+  }
+);
+
+TextInput.displayName = 'TextInput';
 
 export default TextInput;
