9e4f1ed57cd9fd977761ded4cac047a6cb50989a
laodeaksarr <akhsarodhe@gmail.com>
2022-09-11 06:11:29 +0000

    Skeleton components

---
+++ src/components/Skeleton/Skeleton.types.ts
@@ -0,0 +1,7 @@
+export interface SkeletonProps {
+  /** Should skeleton overlay be displayed */
+  visible?: boolean;
+
+  /** If Skeleton is a circle, it's width and border-radius will be equal to height */
+  circle?: boolean;
+}
